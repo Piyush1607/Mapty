e3dc205551f1d98b4db37ae6a7cc80d6986b1bf4
Piyush1607 <2019301@iiitdmj.ac.in>
2021-07-22 18:08:43 +0530

    adding starter files

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// using geolocation API
+// we can pass the position parameter like in eventlistener 
+
+if(navigator.geolocation) // if this exists
+navigator.geolocation.getCurrentPosition(function(position){
+    console.log(position);
+    const {latitude,longitude}=position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+},function(){
+    alert(`can't access location`)
+})
+
+// the first callback is called on success and the second is called if error
+// success if browser gets coordinates
